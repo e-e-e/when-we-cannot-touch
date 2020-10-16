fd50a936f1320543ab90354e7d795035bf0c43cb
Benjamin Forster <benjamin.forster@reaktor.com>
2020-10-16 22:24:27 +0300

    Add google sheets as database for storing collected information

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
+require('dotenv').config()
+
 import Express from 'express'
 import path from 'path'
-
+import bodyParser from 'body-parser'
+import { GoogleSpreadsheet } from 'google-spreadsheet'
 const PORT = 8080
 
-const app = Express()
+async function addItem(data: {
+  name: string
+  street: string
+  city: string
+  state: string
+  postcode: string
+  email: string
+}) {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID || '')
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
+    private_key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  })
+  await doc.loadInfo()
+  const sheet = doc.sheetsByIndex[0]
+  await sheet.addRow([Math.random()])
+}
 
+const app = Express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(Express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {
-  res.send('Hellos')
+
+app.post('/info', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
+  // addItem()
+  //   .then(() => {
+  //     res.sendStatus(200)
+  //   })
+  //   .catch((e) => {
+  //     console.log(e)
+  //     res.sendStatus(500)
+  //   })
 })
 
 app.listen(PORT, () => {
