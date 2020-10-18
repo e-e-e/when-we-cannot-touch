1cd5f02ff56bb85f827a8d6370a774658b06ac4c
Benjamin Forster <benjamin.forster@reaktor.com>
2020-10-18 21:28:23 +0300

    add logger + helmet; client submit logic

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,35 @@
-console.log('we are running');
+let submitting = false
 
-document.body.append('ok this works')
+function processForm(e: Event) {
+  e.preventDefault()
+  if (submitting) return
+  if (!(e.target instanceof HTMLFormElement)) return
+  submitting = true
+  const data = new FormData(e.target)
+  console.log(data)
+  fetch('/info', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: data.get('name'),
+      street: data.get('street'),
+      city: data.get('city'),
+      state: data.get('state'),
+      postcode: data.get('postcode'),
+      email: data.get('email'),
+    }),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      submitting = false
+      if (result) return
+    })
+    .catch((e) => {
+      submitting = false
+    })
+  return false
+}
 
+document.getElementById('form')?.addEventListener('submit', processForm)
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 require('dotenv').config()
 
 import Express from 'express'
+import helmet from 'helmet/dist'
 import path from 'path'
 import bodyParser from 'body-parser'
-import { GoogleSpreadsheet } from 'google-spreadsheet'
+import {GoogleSpreadsheet} from 'google-spreadsheet'
+import * as winston from 'winston'
+import * as expressWinston from 'express-winston'
+
 const PORT = 8080
 
-async function addItem(data: {
+type InputFormData = {
   name: string
   street: string
   city: string
   state: string
   postcode: string
   email: string
-}) {
+}
+
+async function addItem(data: InputFormData) {
   const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID || '')
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || '',
@@ -24,15 +30,38 @@ async function addItem(data: {
   await sheet.addRow([Math.random()])
 }
 
+function validateInput(data: InputFormData): string | null {
+  // TODO: add length validation
+  if (!data.name) return 'Requires name'
+  if (!data.street) return 'Requires street'
+  if (!data.city) return 'Requires city'
+  if (!data.postcode && data.postcode.length > 0 && data.postcode.length < 5)
+    return 'Requires valid postcode'
+  if (!data.email) return 'Requires valid email'
+  return null
+}
+
 const app = Express()
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet())
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: process.env.NODE_ENV === 'development' ? winston.format.cli() : winston.format.json(),
+    meta: true,
+  }),
+)
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(Express.static(path.join(__dirname, 'public')))
 
 app.post('/info', (req, res) => {
   console.log(req.body)
-  res.sendStatus(200)
-  // addItem()
+  const error = validateInput(req.body)
+  if (error) {
+    return res.status(400).json({status: 'error', error})
+  }
+  res.status(200).send({status: 'ok'})
+  // addItem(validData)
   //   .then(() => {
   //     res.sendStatus(200)
   //   })
