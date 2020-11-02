1f4fe177a6a353670c3d39bd9edae38273736d49
Benjamin Forster <benjamin.forster@reaktor.com>
2020-11-02 21:20:20 +0200

    stager entry to include image load time

--- src/client.ts
+++ src/client.ts
@@ -77,18 +77,18 @@ function processForm(e: Event) {
 
 document.getElementById('form')?.addEventListener('submit', processForm)
 
-for (let i =1; i <= 7; i ++ ) {
-  setTimeout(() => {
-    createSecurity(i)
-  }, 500 * i)
+function wait(t: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, t)
+  })
 }
 
 function addImageToDom(img: HTMLImageElement) {
   const div = document.createElement('div')
   div.appendChild(img)
   div.style.position = 'absolute'
-  div.style.top = `${(Math.random() * 80) + 10}%`
-  div.style.left = `${(Math.random() * 80) + 10}%`
+  div.style.top = `${Math.random() * 80 + 10}%`
+  div.style.left = `${Math.random() * 80 + 10}%`
   div.style.zIndex = '-1'
   div.className = 'security'
   const rotation = Math.random() * 360
@@ -98,7 +98,26 @@ function addImageToDom(img: HTMLImageElement) {
 }
 
 function createSecurity(index: number) {
-  const img = new Image()
-  img.onload = () => addImageToDom(img)
-  img.src = `./imgs/WWCT_01_Security-Window${index}.png`
+  return new Promise((resolve, reject) => {
+    console.log('create')
+    const img = new Image()
+    img.onload = () => {
+      addImageToDom(img)
+      resolve()
+    }
+    img.onerror = (e) => {
+      console.log(e)
+      reject()
+    }
+    img.src = `./imgs/WWCT_01_Security-Window${index}.png`
+  })
 }
+
+function startAnimation() {
+  let promises: Promise<any> = Promise.resolve()
+  for (let i = 1; i <= 7; i++) {
+    promises = promises.then(() => createSecurity(i)).then(() => wait(750))
+  }
+}
+
+startAnimation()
